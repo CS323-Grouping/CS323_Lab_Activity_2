f68f1a5d45dea55a879026b08fc415a3f64c23e9
Tion <tion.jamesdominic@gmail.com>
2026-02-08 10:24:36 +0800

    added template 2

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import threading
+from multiprocessing import Process
 def compute_gwa(grades):
     gwa = sum(grades) / len(grades)
     print(f"[Thread] Calculated GWA: {gwa}")
@@ -13,3 +14,18 @@ for grade in grades_list:
 
 for t in threads:
     t.join()
+
+def compute_gwa_mp(grades):
+    gwa = sum(grades) / len(grades)
+    print(f"[Process] Calculated GWA: {gwa}")
+
+grades_list = [85, 90, 78, 92] # Replace with user input
+
+processes = []
+for grade in grades_list:
+    p = Process(target=compute_gwa_mp, args=([grade],))
+    processes.append(p)
+    p.start()
+
+for p in processes:
+    p.join()
